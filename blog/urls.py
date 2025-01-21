fe06a6ae73ebacb090afd669a3ef772ac6700fd8
meghanamanoj24 <meghanamanoj424@gmail.com>
2025-01-21 12:38:52 +0530

    commiting

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views  # Import your views from the blog app
 
 urlpatterns = [
-    path('', views.home, name='home'),  # Home page
+    path('', views.post_list, name='post_list'),  # Home page
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
-def home(request):
-    return render(request, 'blog/post_list.html')  # Replace with your template
+def post_list(request):
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
